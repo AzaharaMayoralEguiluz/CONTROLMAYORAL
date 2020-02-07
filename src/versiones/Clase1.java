bb4cab0fd284b255d1d4a94f5f57a2b2d9be3c63
dam <dam@172.26.39.99>
2020-02-07 09:14:22 +0100

    PrimeraSubida

---
+++ src/versiones/Clase1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package versiones;
+
+/**
+ *
+ * @author dam
+ */
+public class Clase1 {
+
+    public Clase1() {
+        
+        System.out.print("Primera Linea");
+        
+        System.out.print("Segunda Linea");
+    }
+    
+  
+}
